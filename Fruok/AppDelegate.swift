02ac95461092ce3a3d3f239d8cfa10f4931bbb27
Sergio Daniel Lozano <zheref@icloud.com>
2017-04-05 17:53:38 -0500

    Initial Commit

---
+++ Fruok/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Fruok
+//
+//  Created by Sergio Daniel L. García on 4/5/17.
+//  Copyright © 2017 Sergio Daniel L. García. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
