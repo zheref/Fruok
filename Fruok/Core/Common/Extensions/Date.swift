cb24c2c3b830fba892e21670d59efa462b189ce4
Sergio Daniel Lozano <zheref@icloud.com>
2017-05-29 12:22:15 -0400

    Add Date extension and playground

---
+++ Fruok/Core/Common/Extensions/Date.swift
@@ -0,0 +1,14 @@
+//
+//  Date.swift
+//  Fruok
+//
+//  Created by Sergio Daniel L. García on 5/29/17.
+//  Copyright © 2017 Sergio Daniel L. García. All rights reserved.
+//
+
+import Foundation
+
+extension Date {
+    
+    
+}
---
+++ Fruok/MyPlayground.playground/Contents.swift
@@ -0,0 +1,8 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+var str = "Hello, playground"
+
+let secondsFrom1970: TimeInterval = 1495318001.99009
+let date = Date(timeIntervalSince1970: secondsFrom1970)
